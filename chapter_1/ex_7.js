6d6eabb7064160a66fc7ff98b6ad309ee0c36132
Anwar3006 <anwarsadat.d2@gmail.com>
2024-01-04 19:33:00 +0000

    take input from udp client, process and store in a file

--- chapter_1/ex_7.js
+++ chapter_1/ex_7.js
@@ -6,8 +6,41 @@
  * create client, UDP client -> sends data -> server, UDP server -> receives data, processes it, writes it to file.
  */
 const udp = require("dgram");
+const path = require("path");
+const fs = require("fs");
 
 //Server
 const server = udp.createSocket("udp4");
 server.bind(4040);
-server.on("message", (message) => {});
+//create file and persist message inside the file
+const filePath = path.join(__dirname + "/" + "storedUDPMessages.txt");
+if (!fs.existsSync(filePath)) {
+  fs.writeFileSync(filePath, "", "utf-8");
+  console.log("File Storage Created!");
+}
+
+const outputStream = fs.createWriteStream(filePath, {
+  flags: "a",
+  mode: 0o666,
+});
+
+server.on("message", (message, info) => {
+  let serverResponse = "message received: " + message + " from " + info.address;
+  server.send(serverResponse, info.port, info.address, (err) => {
+    if (err) console.error(err);
+  });
+
+  //persist message in file
+  outputStream.write(
+    `${info.address}:${info.port} --> ${message.toString().toUpperCase()}\n`
+  );
+});
+server.on("close", () => outputStream.end());
+
+//client
+const client = udp.createSocket("udp4");
+process.stdin.on("data", (data) => {
+  client.send(data, 4040, "localhost", (err) => {
+    if (err) throw err;
+  });
+});
