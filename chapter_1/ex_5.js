2ce9f3e4bb41e1421cd0a0d8920fd996463b42b0
Anwar3006 <anwarsadat.d2@gmail.com>
2024-01-04 10:06:00 +0000

    weakmaps

--- chapter_1/ex_5.js
+++ chapter_1/ex_5.js
@@ -1,7 +1,21 @@
 // use of WeakMaps
 const owners = new WeakMap();
-const tasks = {
-    title: "Create blog"
-}
+let tasks = {
+  title: "Create blog",
+};
 
 owners.set(tasks, "John");
+
+const owner = (task) => {
+  if (owners.has(task)) {
+    return console.log("owner is: " + owners.get(task)); //John
+  }
+  console.log("no owner");
+};
+
+// owner(tasks); // owner is: John
+
+//remove reference to the key object tasks
+tasks = null;
+
+owner(tasks); // no owner, now tasks variable has no reference to the tasks object inside the weakmap so gc removes it
